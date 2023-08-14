5076c8c5653514ea972ad4494605c4e60e91cb83
Marcel NevesTeixeira <marcel.alunos@cfp127aluno.local>
2023-08-14 15:21:10 -0300

    Calcular a média em Py

---
+++ app.py
@@ -0,0 +1,20 @@
+# comentario
+  
+# Exibe uma mensagem na tela 
+
+print('######################################################')
+print('#################### Exercício 01 ####################')
+print('######################################################')
+
+print('Exercício 01 - Somando dois números')
+
+# solicita a entrada de um valor para o usuario
+nome = input('Boa tarde,Qual é seu nome??')
+
+valor1 = int(input('Digite o primeiro número: '))
+
+valor2 = int(input('Digite o segundo número: ')) 
+# processamento dos dados
+result = valor1 + valor2
+#saida de dados
+print('O resultado é', result)
